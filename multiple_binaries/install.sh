2cc09b0582c4fb5db2d8ebd52c47e5886810ef05
Juraj Žittňanský <juraj.zittnansky@firma.seznam.cz>
2023-09-10 07:37:10 +0200

    updates multiple_binaries/

--- multiple_binaries/install.sh
+++ multiple_binaries/install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-### kustomize
+read -p "Enter architecture: " ARCHITECTURE
+
+# kustomize
+# releases: https://github.com/kubernetes-sigs/kustomize/releases
 
 KUSTOMIZE_VERSION=5.1.1;
 
-curl -sSLO https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz;
+curl -sSLO https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_${ARCHITECTURE}.tar.gz;
 
-tar -xvf kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz; mv kustomize kustomize_v${KUSTOMIZE_VERSION}; rm -rf *.tar.gz;
+tar -xvf kustomize_v${KUSTOMIZE_VERSION}_linux_${ARCHITECTURE}.tar.gz; mv kustomize kustomize_v${KUSTOMIZE_VERSION}; rm -rf *.tar.gz;

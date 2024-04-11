2aba0234c6518e530e1faf8239342e65bf3ec3f4
Juraj Žittňanský <juraj.zittnansky@firma.seznam.cz>
2024-04-11 18:24:02 +0200

    Install setup / fix bash sript

--- install_setup/README.md
+++ install_setup/README.md
@@ -8,14 +8,14 @@ Binaries list:
 
 |     Binary Name       | Release Version |
 | --------------------  | -------------   |
-|     k9s               |     0.27.4      |
-|     Dive              |     0.11.0      |
-|     Trivy             |     0.44.0      |
+|     k9s               |     0.32.4      |
+|     Dive              |     0.12.0      |
+|     Trivy             |     0.50.1      |
 |     kubectx           |     0.9.5       |
 |     kubens            |     0.9.5       |
-|     kubectl           |     1.27.4      |
-|     Docker Engine     |     24.0.2      |
-|     Docker Compose    |     2.20.2      |
+|     kubectl           |     1.29.2      |
+|     Docker Engine     |     26.0.1      |
+|     Docker Compose    |     2.26.1      |
 
 # Tools List
 
@@ -25,6 +25,18 @@ Binaries list:
 |     [Dive](https://github.com/wagoodman/dive)        
 |     [Trivy](https://github.com/aquasecurity/trivy)
 |     [kubectx + kubens](https://github.com/ahmetb/kubectx)
-|     [kubectl](https://github.com/ahmetb/kubectx)
+|     [kubectl](https://github.com/kubernetes/kubectl)
 |     [Docker Engine](https://github.com/docker)   
 |     [Docker Compose](https://github.com/docker/compose)
+
+# Tools Releases
+
+|    Binary Name                                                    
+| --------------------                                              
+|     [k9s](https://github.com/derailed/k9s/releases)                        
+|     [Dive](https://github.com/wagoodman/dive/releases)        
+|     [Trivy](https://github.com/aquasecurity/trivy/releases)
+|     [kubectx + kubens](https://github.com/ahmetb/kubectx/releases)
+|     [kubectl](https://kubernetes.io/releases/)
+|     [Docker Engine](https://docs.docker.com/engine/release-notes/26.0/)   
+|     [Docker Compose](https://github.com/docker/compose/releases)
--- install_setup/script.sh
+++ install_setup/script.sh
@@ -14,13 +14,13 @@ sudo apt-get install -y wget curl bmon htop vim nmap
 #Common tools:
 
 #k9s
-K9S_VERSION=0.27.4; curl -sSLO https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_Linux_${ARCHITECTURE}.tar.gz; tar -xvf k9s_Linux_${ARCHITECTURE}.tar.gz; rm -rf LICENSE README.md; sudo cp k9s /usr/local/bin; rm -rf k9s; rm -rf k9s_*.tar.gz
+K9S_VERSION=0.32.4; curl -sSLO https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_Linux_${ARCHITECTURE}.tar.gz; tar -xvf k9s_Linux_${ARCHITECTURE}.tar.gz; rm -rf LICENSE README.md; sudo cp k9s /usr/local/bin; rm -rf k9s; rm -rf k9s_*.tar.gz
 
 #Dive
-DIVE_VERSION=0.11.0;  curl -sSLO https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_${ARCHITECTURE}.deb && sudo dpkg -i dive_${DIVE_VERSION}_linux_${ARCHITECTURE}.deb; rm -rf dive*
+DIVE_VERSION=0.12.0;  curl -sSLO https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_${ARCHITECTURE}.deb && sudo dpkg -i dive_${DIVE_VERSION}_linux_${ARCHITECTURE}.deb; rm -rf dive*
 
 #Trivy
-TRIVY_VERSION=0.44.0; curl -sSLO https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-${ARCHITECTURE4}.deb && sudo dpkg -i trivy_${TRIVY_VERSION}_Linux-${ARCHITECTURE4}.deb; rm -rf trivy*.deb
+TRIVY_VERSION=0.50.1; curl -sSLO https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-${ARCHITECTURE4}.deb && sudo dpkg -i trivy_${TRIVY_VERSION}_Linux-${ARCHITECTURE4}.deb; rm -rf trivy*.deb
 
 #Kubens + Kubectx
 KUBECTX_VERSION=0.9.5; curl -sSLO https://github.com/ahmetb/kubectx/releases/download/v${KUBECTX_VERSION}/kubectx_v${KUBECTX_VERSION}_linux_${ARCHITECTURE3}.tar.gz; tar -xvf kubectx_v${KUBECTX_VERSION}_linux_${ARCHITECTURE3}.tar.gz; rm -rf LICENSE; sudo cp kubectx /usr/local/bin/; rm -rf kubectx*
@@ -28,12 +28,14 @@ KUBECTX_VERSION=0.9.5; curl -sSLO https://github.com/ahmetb/kubectx/releases/dow
 KUBENS_VERSION=0.9.5; curl -sSLO https://github.com/ahmetb/kubectx/releases/download/v${KUBENS_VERSION}/kubens_v${KUBENS_VERSION}_linux_${ARCHITECTURE3}.tar.gz; tar -xvf kubens_v${KUBENS_VERSION}_linux_${ARCHITECTURE3}.tar.gz; rm -rf LICENSE; sudo cp kubens /usr/local/bin/; rm -rf kubens*
 
 #kubectl
-KUBECTL_VERSION=1.28.1; curl -sSLO https://dl.k8s.io/release/v${KUBECTL_VERSION}/bin/linux/${ARCHITECTURE}/kubectl
+KUBECTL_VERSION=1.29.2; curl -sSLO https://dl.k8s.io/release/v${KUBECTL_VERSION}/bin/linux/${ARCHITECTURE}/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl; rm -rf kubectl
 
-# Install My repo w/ Starship
-git clone https://github.com/jz543fm/starship-conf.git
-cd starship-conf/; chmod 750 install*; ./install_bash.sh; cd ../; rm -rf starship-conf;
+#Docker compose
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+DOCKER_COMPOSE_VERSION=2.26.1; curl -SL https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-${ARCHITECTURE2} -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 #Docker Engine
 curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh get-docker.sh; rm -rf get-docker.sh
@@ -43,8 +45,5 @@ curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh get-docker.sh; rm -r
         newgrp docker
 
 rm -rf get-docker.sh
-
-#Docker compose
-mkdir -p ~/.docker/cli-plugins/; DOCKER_COMPOSE=2.20.2; curl -SL        download/v${DOCKER_COMPOSE}/docker-compose-linux-${ARCHITECTURE2} -o ~/.docker/cli-plugins/docker-compose; chmod +x ~/.docker/cli-plugins/docker-compose #permission
 
 docker compose version #verify

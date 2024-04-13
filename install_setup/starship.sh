3cbe8418da109c09f8de4030c31336c6cf8f27b5
Juraj Žittňanský <juraj.zittnansky@firma.seznam.cz>
2024-04-13 10:59:53 +0200

    refactos

--- install_setup/starship.sh
+++ install_setup/starship.sh
@@ -3,3 +3,4 @@
 # Install My repo w/ Starship
 git clone https://github.com/jz543fm/starship-conf.git
 cd starship-conf/; chmod 750 install*; ./install_bash.sh; cd ../; rm -rf starship-conf;
+curl -sS https://starship.rs/install.sh | sudo sh #Linux

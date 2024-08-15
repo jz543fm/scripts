67990921bb117f48bd14f2ad6e2c579af8dbc819
Žittňanský, Juraj <juraj.zittnansky@firma.seznam.cz>
2024-08-15 19:25:07 +0200

    refactors

--- install_setup/linux.sh
+++ install_setup/linux.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+uptime
+
 #Get OS
 uname -a
 
--- install_setup/starship.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Install My repo w/ Starship
-git clone https://github.com/jz543fm/starship-conf.git
-cd starship-conf/; chmod 750 install*; ./install_bash.sh; cd ../; rm -rf starship-conf;
-curl -sS https://starship.rs/install.sh | sudo sh #Linux
